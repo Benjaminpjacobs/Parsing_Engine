31a96e727e442fd7c5a78531bc3ff63aa86ca23f
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-02-15 13:14:42 -0700

    Create parsing_engine and methods library

---
+++ lib/markdown_methods.rb
@@ -0,0 +1,136 @@
+  def split_by_double(input)
+    input.map!{|x| x.split(/(\n\n)/)}.flatten!
+  end
+
+  def split_by_single(input)
+    input.map! do |x|
+      if x.include?("\n") && x != "\n\n"
+        x.split(/(\n)/)
+      else
+        x
+      end
+    end
+    input.flatten!
+  end
+  
+  def heading6(input)
+    if input.include?("###### ")
+      input["###### "] = "<h6>"
+      input.insert(-1, "</h6>")
+    else
+      input 
+    end  
+  end
+
+  def heading5(input)
+    if input.include?("##### ")
+      input["##### "] = "<h5>"
+      input.insert(-1, "</h5>")
+    else
+      input 
+    end
+  end
+
+  def heading4(input)
+    if input.include?("#### ")
+      input["#### "] = "<h4>"
+      input.insert(-1, "</h4>")
+    else
+      input 
+    end
+  end
+
+  def heading3(input)
+    if input.include?("### ")
+      input["### "] = "<h3>"
+      input.insert(-1, "</h3>")
+    else
+      input 
+    end
+  end
+
+  def heading2(input)
+    if input.include?("## ")
+      input["## "] = "<h2>"
+      input.insert(-1, "</h2>")
+    else
+      input 
+    end
+  end
+
+  def heading1(input)
+    if input.include?("# ")
+      input["# "] = "<h1>"
+      input.insert(-1, "</h1>")
+    else
+      input 
+    end
+  end
+
+  def headings(input)
+      input.map do |x|
+      heading6(x); heading5(x); heading4(x); 
+      heading3(x); heading2(x); heading1(x); 
+      end
+  end
+
+  def paragraphs(input)
+      input.each_with_index.map do |x, idx|
+        if x.include?("\n") == true || x.include?("<h") == true
+          x
+        elsif input[idx-1] == "\n\n" && input[idx+1] == "\n"
+          x.insert(0, "<p>\n")
+        elsif input[idx-1]=="\n" && input[idx-2].include?("<h") && (idx+1) > (input.length-1)
+          x.insert(0, "<p>\n"); x.insert(-1, "\n</p>")
+        elsif input[idx-1]== "\n" && (idx+1) > (input.length-1)
+          x.insert(-1, "\n</p>")
+        elsif input[idx-1]=="\n" && input[idx+1]=="\n"
+          x
+        else
+          x.insert(0, "<p>\n"); x.insert(-1, "\n</p>")
+        end
+      end      
+  end
+
+  def emphasis_open(input)
+      input.split.each{|x| x.sub!("**", "<em>") if x[0..1] == "**"}
+  end
+
+  def emphasis_close(input)
+      input.split.each {|x| x.sub!("**", "</em>") if x[-3..-1].nil? != true && x[-3..-1].include?("**")}
+  end
+  
+  def emphasis(input)
+    input.map!{ |x| x.include?("**") ? emphasis_open(x): x}
+    recombine(input)
+    input.map!{ |x| x.include?("**") ? emphasis_close(x): x}
+    recombine(input)
+  end
+
+  def strong_open(input)
+      input.split.each{|x| x.sub!("*", "<strong>") if x[0] == "*"}
+  end
+
+  def strong_close(input)
+      input.split.each {|x| x.sub!("*", "</strong>") if x[-2..-1].nil? != true && x[-2..-1].include?("*")}
+  end
+
+  def strong(input)
+    input.map!{ |x| x.include?("*") ? strong_open(x): x}
+    recombine(input)
+    input.map!{ |x| x.include?("*") ? strong_close(x): x}
+    recombine(input)
+  end
+
+
+  def recombine(input)
+    input.map! {|x| x.kind_of?(Array) ? x.join(' ') : x}
+  end
+
+
+  # def strong(input)
+  #   input.gsub!("* ", "</strong> ")
+  #   input.gsub!(" *", " <strong>")
+  #   input.gsub!("*", "<strong>")
+  # end
+
---
+++ parsing_engine.rb
@@ -0,0 +1,27 @@
+require './lib/markdown_methods.rb'
+require 'pry'
+
+unless ARGV.length == 2
+  puts "Not the correct number of arguments"
+  exit
+end
+
+input_file =  ARGV[0]
+destination_file = ARGV[1]
+lines_array = []
+input_file = File.open(input_file, 'r')
+lines_array << input_file.read
+
+split_by_double(lines_array); split_by_single(lines_array); headings(lines_array);
+emphasis(lines_array) ;strong(lines_array); paragraphs(lines_array)
+
+
+output_file = File.open(destination_file, "w")
+output_file.write(lines_array.join)
+input_file.close
+output_file.close
+
+puts "converted #{ARGV[0]} (#{File.foreach(ARGV[0]).count} lines) 
+to #{ARGV[1]} (#{File.foreach(ARGV[1]).count} lines) 
+at #{Time.now}" 
+
