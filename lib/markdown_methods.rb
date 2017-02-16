ee812d482956fdeea15a518376975bccd60828f0
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-02-16 10:36:50 -0700

    Save paragraph methods

--- lib/markdown_methods.rb
+++ lib/markdown_methods.rb
@@ -74,26 +74,6 @@
       end
   end
 
-  # def paragraphs(input)
-  #     input.each_with_index.map do |x, idx|
-  #       if x.include?("\n") == true || x.include?("<h") == true || x.include?("<li>")
-  #         x
-  #       elsif input[idx-1] == "\n\n" && input[idx+1] == "\n"
-  #         x.insert(0, "<p>\n")
-  #       elsif input[idx-1]=="\n" && input[idx-2].include?("<h") && (idx+1) > (input.length-1)
-  #         x.insert(0, "<p>\n"); x.insert(-1, "\n</p>")
-  #       elsif input[idx-1]== "\n" && (idx+1) > (input.length-1)
-  #         x.insert(-1, "\n</p>")
-  #       elsif input[idx-1] == "\n" && input[idx+1] == "\n\n"
-  #         x.insert(-1, "\n</p>")
-  #       elsif input[idx-1]=="\n" && input[idx+1]=="\n"
-  #         x
-  #       else
-  #         x.insert(0, "<p>\n"); x.insert(-1, "\n</p>")
-  #       end
-  #     end      
-  # end
-
   def paragraphs(input)
       input.each_with_index.map do |x, idx|
         if x.include?("\n") == true || x.include?("<h") == true || x.include?("<li>")
