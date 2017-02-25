3d1e0004135018b449e272e4ab432e23ecee8d44
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-02-25 13:58:08 -0700

    Update image methods

--- lib/images.rb
+++ lib/images.rb
@@ -26,26 +26,34 @@ class Image
     str.include?('"') ? str.sub!('"', '" title="') : str.sub!("'", "title='") 
     str.sub!(')', ' ') 
   end
+
+  def total_tag_wtih_title(str)
+    str = split_reverse(str)
+    add_alt_tag(str)
+    add_title_tag(str)
+    add_src_tag(str)
+    str
+  end
+
+  def total_tag(str)
+    str = split_reverse(str)
+    add_alt_tag(str)
+    add_src_tag(str)
+    str
+  end
   
   def convert_image_tag(str)
     if line_is_image_tag(str) == true && has_opt_title(str) == true
-      str = split_reverse(str)
-      add_alt_tag(str)
-      add_title_tag(str)
-      add_src_tag(str)
-      str
+      total_tag_wtih_title(str)
     elsif line_is_image_tag(str) == true 
-      str = split_reverse(str)
-      add_alt_tag(str)
-      add_src_tag(str)
-      str
+      total_tag(str)
     else
       str
     end
   end
 
   def search_for_image(str_arr)
-    str_arr.map!{|str| convert_image_tag(str)}
+    str_arr.map!{ |str| convert_image_tag(str) }
   end
 
 end
