d2523e271407b2f606cc38f4452f05b44268afbd
Benjamin Jacobs <benjaminpjacobs@gmail.com>
2017-02-25 10:37:09 -0700

    Fix spacing, add line module

--- lib/paragraphs.rb
+++ lib/paragraphs.rb
@@ -1,23 +1,23 @@
+require './lib/linemodule.rb'
+
 class Para
+  include Lines
 
   def paragraphs(str_arr)
     str_arr.each_with_index.map do |str, idx|
-      prev_line = str_arr[idx - 1]
-      next_line = str_arr[idx + 1]
-      prev_sec_line = str_arr[idx - 2]
       if str.include?("\n") == true || str.include?("<h") == true || str.include?("<li>")
         str
-      elsif prev_line == "\n\n" && next_line == "\n\n"
+      elsif prev_line(str_arr, idx, 1) == "\n\n" && next_line(str_arr, idx, 1) == "\n\n"
         str.insert(0, "<p>"); str.insert(-1, "</p>")
-      elsif prev_line == "\n\n" && next_line == "\n"
+      elsif prev_line(str_arr, idx, 1) == "\n\n" && next_line(str_arr, idx, 1) == "\n"
         str.insert(0, "<p>")
-      elsif prev_line == "\n" && next_line == "\n\n"
+      elsif prev_line(str_arr, idx, 1) == "\n" && next_line(str_arr, idx, 1) == "\n\n"
         str.insert(-1, "</p>")
-      elsif prev_line == "\n" && prev_sec_line.include?("<h")
+      elsif prev_line(str_arr, idx, 1) == "\n" && prev_line(str_arr, idx, 2).include?("<h")
         str.insert(0, "<p>"); str.insert(-1, "</p>")
-      elsif prev_line == "\n" && (idx+1) > (str_arr.length-1)
+      elsif prev_line(str_arr, idx, 1) == "\n" && (idx+1) > (str_arr.length-1)
         str.insert(-1, "</p>")
-      elsif prev_line == "\n" && next_line == "\n"
+      elsif prev_line(str_arr, idx, 1) == "\n" && next_line(str_arr, idx, 1) == "\n"
         str
       else
         str.insert(0, "<p>")
